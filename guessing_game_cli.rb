4a4806e06208d4ca909e047661704faf3f6c3c96
Dawit Gizaw <gizawdawit123@gmail.com>
2020-02-20 04:42:06 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,8 +1,9 @@
 def run_guessing_game
   comp_guess = 1 + rand(6)
+  comp_guesss = "#{comp_guess}"
   user_num = gets.chomp
   
-  if user_num == comp_guess
+  if user_num == comp_guesss
     puts "You guessed the correct number!"
   elsif user_num == "exit"
     puts "Goodbye!"
