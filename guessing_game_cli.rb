82c5f88eded8a2d6b8ba957eafc03c3bab16e4fc
Dawit Gizaw <gizawdawit123@gmail.com>
2020-02-20 04:21:09 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,6 +4,10 @@ def running_guess_game
   guess = gets.chomp
   if num == guess
     puts "You guessed the correct number!"
+  elsif guess == "exit"
+    puts "Goodbye!"
   else
+    puts "Sorry! The computer guessed " + num
+  end
     
 end
